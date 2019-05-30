d6d158c4d910a7d6003e9cc5ef4587b1aa0384f2
Zdream <helloo@foxmail.com>
2019-05-30 23:20:22 +0800

    更新攀爬梯子的逻辑

--- core/src/zdream/rockchronicle/core/module/action/ClimbModule.java
+++ core/src/zdream/rockchronicle/core/module/action/ClimbModule.java
@@ -56,13 +56,17 @@ public class ClimbModule extends AbstractModule implements IMovable {
 	int haltRemain;
 	
 	/**
-	 * 补充参数, 本帧是否需要从梯子的顶端下降到梯子上
+	 * 补充参数, 本步时间内是否需要从梯子的顶端下降到梯子上
 	 */
 	boolean lower;
 	/**
 	 * 如果上面的 lower 为 true 时, 这里写出其横纵坐标
 	 */
 	float lowerX, lowerY;
+	/**
+	 * 指示本步时间内是否从 climbing = 0 的状态变成附到梯子上
+	 */
+	boolean adhere;
 	
 	public ClimbModule(CharacterEntry parent) {
 		super(parent);
@@ -75,13 +79,13 @@ public class ClimbModule extends AbstractModule implements IMovable {
 		JsonValue v = value.get("climbParam");
 		float fv = v.getFloat("velocity");
 		climbVelocity = fv * TIME_STEP;
+		setSituation("climb.param.velocity", new JsonValue(climbVelocity));
 		
 		parent.addSubscribe("ctrl_axis", this);
 		parent.addSubscribe("ctrl_motion", this);
 		parent.addSubscribe("open_fire", this);
 
 		parent.getBoxModule().addMovable(this, 20);
-		setClimbParam();
 	}
 
 	@Override
@@ -92,13 +96,6 @@ public class ClimbModule extends AbstractModule implements IMovable {
 	@Override
 	public int priority() {
 		return 0x81;
-	}
-	
-	/**
-	 * 长期参数部分
-	 */
-	public void setClimbParam() {
-		setSituation("climb.param.velocity", new JsonValue(climbVelocity));
 	}
 	
 	/**
@@ -119,6 +116,10 @@ public class ClimbModule extends AbstractModule implements IMovable {
 			return;
 		}
 		
+		// 该方法需要处理的是:
+		// 1. 判断角色是否还在梯子上, 如果不是, climbing 设置为 0;
+		// 2. 判断角色是否将附到梯子上. 如果是, 将在 move 方法中修改 climbing, 该方法不作修改
+		
 		// 被攻击硬直时, 后面的判断都不用继续了
 		boolean stiffness = getBoolean("state.stiffness", false);
 		if (stiffness) { // 被攻击硬直时, 后面的判断都不用继续了
@@ -130,62 +131,91 @@ public class ClimbModule extends AbstractModule implements IMovable {
 		Box box = parent.getBoxModule().getBox();
 		// 盒子的中心点
 		Vector2 centerPoint = box.getPosition().getCenter(new Vector2());
-		
-		// 检测是否在攀登状态. 取消攀登状态的可能有以下情况: 
-		// 1. 梯子消失 (现在正在检查的)
-		// 2. 被攻击 (前面已经过滤)
-		// 3. 跳下来 (监听事件, 不在这里)
-		// 4. 顺着梯子到底, 站到了平地上
-		
 		// 现在查看这个中心点映射到的是哪个地形
-		byte terrain = world.getTerrain((int) centerPoint.x, (int) centerPoint.y);
-		if (!Terrains.isLadder(terrain) && climbing == 0) {
-			// 还有一种情况, 在梯子顶端按下将下降到下面的梯子上
+		
+		if (climbing == 0) {
+			setClimbState();
 			BoxOccupation occ = box.getOccupation();
 			
-			if (box.gravityDown && occ.ybottomTightly) {
-				if (Terrains.isLadder(world.getTerrain((int) centerPoint.x, occ.ybottom - 1)) && upOrDown == 2) {
-					// 可以向下降到梯子上
-					lower = true;
-					lowerX = ((int) centerPoint.x) + 0.5f;
-					lowerY = occ.ybottom;
-					return;
+			byte terrain = world.getTerrain((int) centerPoint.x, (int) centerPoint.y);
+			if (Terrains.isLadder(terrain)) {
+				// 这个时候如果按了上或者下, 就应该到梯子上了, 除了一种情况: 你在落地时按下
+				if (box.gravityDown && occ.ybottomTightly) {
+					if (world.getTerrain((int) centerPoint.x, occ.ybottom - 1) == TERRAIN_SOLID
+							&& upOrDown == 2) {
+					} else {
+						this.adhere = true;
+					}
+				} else if (!box.gravityDown && occ.ytopTightly) {
+					if (world.getTerrain((int) centerPoint.x, occ.ytop + 1) == TERRAIN_SOLID
+							&& upOrDown == 1) {
+					} else {
+						this.adhere = true;
+					}
+				} else {
+					this.adhere = true;
 				}
-			} else if (!box.gravityDown && occ.ytopTightly) {
-				if (Terrains.isLadder(world.getTerrain((int) centerPoint.x, occ.ytop + 1)) && upOrDown == 1) {
-					// 人是倒着的, 可以向上“降”到梯子上
-					lower = true;
-					lowerX = ((int) centerPoint.x) + 0.5f;
-					lowerY = occ.ytop + 1;
-					return;
+			} else {
+				// 还有一种情况, 在梯子顶端按下将下降到下面的梯子上
+				if (box.gravityDown && occ.ybottomTightly) {
+					if (Terrains.isLadder(world.getTerrain((int) centerPoint.x, occ.ybottom - 1)) && upOrDown == 2) {
+						// 可以向下降到梯子上
+						lower = true;
+						lowerX = ((int) centerPoint.x) + 0.5f;
+						lowerY = occ.ybottom;
+					}
+				} else if (!box.gravityDown && occ.ytopTightly) {
+					if (Terrains.isLadder(world.getTerrain((int) centerPoint.x, occ.ytop + 1)) && upOrDown == 1) {
+						// 人是倒着的, 可以向上“降”到梯子上
+						lower = true;
+						lowerX = ((int) centerPoint.x) + 0.5f;
+						lowerY = occ.ytop + 1;
+					}
 				}
 			}
 			return;
 		}
 		
-		// 顺着梯子到底, 站到了平地上
-		if (Terrains.isLadder(terrain) && climbing == 1) {
-			BoxOccupation occ = box.getOccupation();
+		// 到达这里说明 climbing > 0
+		
+		// 检测是否在攀登状态. 取消攀登状态的可能有以下情况: 
+		// 1. 梯子消失 (现在正在检查的) (需要检查中点和锚点, 一项满足即可)
+		// 2. 被攻击 (前面已经过滤)
+		// 3. 跳下来 (监听事件, 不在这里)
+		// 4. 顺着梯子到底, 站到了平地上 (当 climbing <= 7 时需要检查锚点)
 			
-			if (box.gravityDown && occ.ybottomTightly) {
-				// TODO 其它平地块
-				if (world.getTerrain((int) centerPoint.x, occ.ybottom - 1) == TERRAIN_SOLID) {
-					climbing = 0; // 变成站立
-					parent.getBoxModule().setNextPattern("normal");
-					setClimbState(); return;
+		if (climbing <= 7) {
+			byte terrain = world.getTerrain((int) box.anchor.x, (int) box.anchor.y);
+			// 顺着梯子到底, 站到了平地上 (以后会扩展到站到其它实体)
+			if (Terrains.isLadder(terrain)) {
+				BoxOccupation occ = box.getOccupation();
+				
+				if (box.gravityDown && occ.ybottomTightly) {
+					// TODO 其它平地块
+					if (world.getTerrain((int) centerPoint.x, occ.ybottom - 1) == TERRAIN_SOLID
+							&& upOrDown == 2) {
+						climbing = 0; // 变成站立
+						parent.getBoxModule().setNextPattern("normal");
+						setClimbState(); return;
+					}
+				} else if (!box.gravityDown && occ.ytopTightly) {
+					// TODO 其它平地块
+					if (world.getTerrain((int) centerPoint.x, occ.ytop + 1) == TERRAIN_SOLID
+							&& upOrDown == 1) {
+						climbing = 0; // 变成站立
+						parent.getBoxModule().setNextPattern("normal");
+						setClimbState(); return;
+					}
 				}
-			} else if (!box.gravityDown && occ.ytopTightly) {
-				// TODO 其它平地块
-				if (world.getTerrain((int) centerPoint.x, occ.ytop + 1) == TERRAIN_SOLID) {
+			} else {
+				terrain = world.getTerrain((int) centerPoint.x, (int) centerPoint.y); // 检查中点
+				if (!Terrains.isLadder(terrain)) {
+					// 梯子消失 ?
 					climbing = 0; // 变成站立
 					parent.getBoxModule().setNextPattern("normal");
 					setClimbState(); return;
 				}
 			}
-		}
-		
-		if (climbing >= 2) {
-			setClimbState(); return;
 		}
 		
 		// 这里附加判断:
@@ -195,41 +225,48 @@ public class ClimbModule extends AbstractModule implements IMovable {
 			parent.getBoxModule().setNextPattern("normal");
 			setClimbState(); return;
 		}
+		
+		setClimbState();
 	}
 	
 	@Override
 	public void move(LevelWorld world, Box box, CharacterEntry entry) {
-		if (climbing == 0 && upOrDown == 0) {
+		int ix = (int) box.anchor.x;
+		
+		if (climbing == 0) {
+			if (adhere) {
+				box.setAnchorX(ix + 0.5f);
+				box.setVelocity(0, 0);
+				climbing = 1;
+				setClimbState(); return;
+			} else if (lower) {
+				BoxOccupation occ = box.getOccupation();
+				
+				if (box.gravityDown && occ.ybottomTightly &&
+						Terrains.isLadder(world.getTerrain((int) lowerX, (int) lowerY - 1))) {
+					parent.getBoxModule().setNextPattern("climb_top_1");
+					box.setAnchorX(lowerX);
+					// y 为梯子顶部, (整数), 即不动
+					climbing = 13;
+					setClimbState(); return;
+				}
+				if (!box.gravityDown && occ.ybottomTightly &&
+						Terrains.isLadder(world.getTerrain((int) lowerX, (int) lowerY))) {
+					parent.getBoxModule().setNextPattern("climb_top_1");
+					box.setAnchorX(lowerX);
+					// y 为梯子顶部 (人是倒立的, 可以视作底部), (整数), 即不动
+					climbing = 13;
+					setClimbState(); return;
+				}
+			}
 			return;
 		}
-		
-		// 是否从梯子顶部降落
-		if (lower) {
-			BoxOccupation occ = box.getOccupation();
-			
-			if (box.gravityDown && occ.ybottomTightly && Terrains.isLadder(world.getTerrain((int) lowerX, (int) lowerY))) {
-				parent.getBoxModule().setNextPattern("climb_top_1");
-				box.setAnchorX(lowerX);
-				// y 为梯子顶部, (整数), 即不动
-				climbing = 13;
-				setClimbState(); return;
-			}
-			if (!box.gravityDown && occ.ybottomTightly && Terrains.isLadder(world.getTerrain((int) lowerX, (int) lowerY))) {
-				parent.getBoxModule().setNextPattern("climb_top_1");
-				box.setAnchorX(lowerX);
-				// y 为梯子顶部 (人是倒立的, 可以视作底部), (整数), 即不动
-				climbing = 13;
-				setClimbState(); return;
-			}
-			return;
-		}
-		
-		// 盒子的中心点
-		Vector2 centerPoint = box.getPosition().getCenter(new Vector2());
 		
 		// 特殊攀爬状态
 		if (climbing >= 2) {
 			int last = climbing;
+			box.setVelocity(0, 0);
+			
 			if (this.upOrDown == 1) {
 				climbing = (box.gravityDown) ? climbing + 1 : climbing - 1;
 			} else if (this.upOrDown == 2) {
@@ -240,49 +277,21 @@ public class ClimbModule extends AbstractModule implements IMovable {
 				// 站在楼梯顶端
 				climbing = 0;
 				parent.getBoxModule().setNextPattern("normal");
-				box.setVelocityY(0);
 			} else if (climbing == 1) {
 				// 回到一般状态
 				parent.getBoxModule().setNextPattern("normal");
 				box.setAnchorY((float) Math.ceil(box.anchor.y) - 0.55f);
-				box.setVelocityY(0);
 			} else {
 				if (last == 7 && climbing == 8) {
 					parent.getBoxModule().setNextPattern("climb_top_1");
 					box.setAnchorY((float) Math.ceil(box.anchor.y));
-					box.setVelocityY(0);
 				} else if (last == 8 && climbing == 7) {
 					parent.getBoxModule().setNextPattern("climb_top_0");
 					box.setAnchorY((float) Math.ceil(box.anchor.y) - 0.25f);
-					box.setVelocityY(0);
-				} else {
-					box.setVelocityY(0);
 				}
 			}
 			
-			box.setVelocityX(0);
 			setClimbState(); return;
-		}
-		
-		if (climbing == 0) {
-			// 到了这里说明: 中心点是梯子的地形块, 不在房间外
-			// 原先还不是攀爬状态, 不在硬直状态, 按了上或者下
-			
-			BoxOccupation occ = box.getOccupation();
-			// 如果在梯子的底端, 你站在可站立方块上, 再按下也没有意义
-			if (box.gravityDown && occ.ybottomTightly && upOrDown == 2) {
-				// TODO 其它平地块
-				if (world.getTerrain((int) centerPoint.x, occ.ybottom - 1) == TERRAIN_SOLID) {
-					return;
-				}
-			} else if (!box.gravityDown && occ.ytopTightly && upOrDown == 1) {
-				// TODO 其它平地块
-				if (world.getTerrain((int) centerPoint.x, occ.ytop + 1) == TERRAIN_SOLID) {
-					return;
-				}
-			}
-			
-			setClimbState(); climbing = 1;
 		}
 		
 		// 角色将改变形状 (共 3 个形状, 存储在 StateModule 中的 motion 字段)、
@@ -290,6 +299,7 @@ public class ClimbModule extends AbstractModule implements IMovable {
 		
 		// 角色如果原本不在爬梯子状态, 需要对角色的位置进行调整,
 		// 平移到梯子上;
+		box.setAnchorX(ix + 0.5f);
 		
 		// 将根据角色与梯子顶端的距离来设置爬梯子状态;
 		
@@ -298,12 +308,10 @@ public class ClimbModule extends AbstractModule implements IMovable {
 		boolean yTightly = box.anchor.y == iy;
 		float distance = (iy - box.anchor.y) +
 				(yTightly ?
-				(Terrains.isLadder(world.getTerrain((int) centerPoint.x, iy + 1)) ? 1 : 0) :
-				(Terrains.isLadder(world.getTerrain((int) centerPoint.x, iy)) ? 1 : 0));
+				(Terrains.isLadder(world.getTerrain(ix, iy + 1)) ? 1 : 0) :
+				(Terrains.isLadder(world.getTerrain(ix, iy)) ? 1 : 0));
 		
-		float vx, vy = 0;
-		// vx
-		vx = ((int) centerPoint.x) + 0.5f - centerPoint.x;
+		float vy = 0;
 		
 		// vy
 		if (distance < 0.5f) {
@@ -329,7 +337,7 @@ public class ClimbModule extends AbstractModule implements IMovable {
 			haltRemain --;
 		}
 		
-		box.setVelocityX(vx);
+		box.setVelocityX(0);
 		box.setVelocityY(vy);
 		setClimbState();
 	}
@@ -339,6 +347,7 @@ public class ClimbModule extends AbstractModule implements IMovable {
 		lower = false;
 		lowerX = 0;
 		lowerY = 0;
+		adhere = false;
 	}
 	
 	@Override
